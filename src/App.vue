8908f274e554644bd385554eb193733655ca0266
Victoria Azola <victoria.azola.s@gmail.com>
2024-08-21 13:44:20 -0400

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import CedulaIdentidad from "./components/CedulaIdentidad.vue";
+</script>
+
+<template>
+  <div class="bg">
+    <CedulaIdentidad />
+  </div>
+</template>
+
+<style scoped>
+  .bg {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 60vh;
+    font-size: 10px;
+    font-family: sans-serif;
+  }
+</style>
